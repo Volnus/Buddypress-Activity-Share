d562882a22e0c0233537968317f064b865c64848
Volnus <scotth0407@gmail.com>
2016-10-23 16:11:45 -0500

    a

---
+++ includes/activity-sharing.php
@@ -0,0 +1,38 @@
+<?php
+
+function buddy_social_button_activity_filter() {
+
+    $activity_type = bp_get_activity_type();
+    $activity_link = bp_get_activity_thread_permalink();
+    $activity_title = bp_get_activity_feed_item_title();
+    $plugin_path = plugins_url();
+
+    $buddy_social_facebook = '<a class="new-window social  fa-facebook-square" href="https://www.facebook.com/sharer/sharer.php?t='.$activity_title.'&u=' . $activity_link . '" rel="facebox"></a>';
+
+    $buddy_social_twitter = '<a class="new-window social  fa-twitter-square" href="http://twitter.com/share?text='.$activity_title.'&url=' . $activity_link . '" rel="twitter"></a>';
+
+    $buddy_social_google = '<a class="new-window social fa-google-plus-square" href="https://plus.google.com/share?url=' . $activity_link . '" rel="google-plus"></a>';
+  
+    $buddy_social_tumblr = '<a class="new-window social fa-tumblr-square" href="http://www.tumblr.com/share/link?url=' . $activity_link . '" rel="tumblr"></a>';
+  
+    $buddy_social_email = '<a class="general fa-envelope" href="mailto:?body='.$activity_title .' ' . $activity_link . '" rel="nofollow"></a>';
+ 	
+  	$buddy_social_addthis = '<a class="general fa-plus-square" href="https://www.addthis.com/bookmark.php?source=bx32nj-1.0&v=300&url='.$activity_title .' ' . $activity_link . '" rel="nofollow"></a>';
+
+    ?><span class="bp-social-button">
+<a class="button item-button bp-secondary-action buddypress-social-button" rel="nofollow">Share</a></span>
+    
+    <div class="social-buttons <?php $activity_type ?>" style="display: none;">
+            <?php echo "$buddy_social_facebook"; ?>
+            <?php echo "$buddy_social_twitter"; ?>
+            <?php echo "$buddy_social_google"; ?>
+            <?php echo "$buddy_social_tumblr"; ?>
+            <?php echo "$buddy_social_email"; ?>
+            <?php echo "$buddy_social_addthis"; ?>
+    </div>
+
+    <?php
+}
+add_action('bp_activity_entry_meta', 'buddy_social_button_activity_filter', 999);
+
+?>
---
+++ loader.php
@@ -0,0 +1,45 @@
+<?php 
+/*
+ * Plugin Name: Buddypress Activity Share
+ * Plugin URI: 
+ * Description: Adds social share buttons to BuddyPress activity pages and member profiles. (Very well optimized)
+ * Version: 1.0
+ * Author: Scott Hartley
+ * Author URI: https://arcademill.com
+ * Based On: BuddyPress Social
+ */
+
+function bp_social_init() {
+	require( dirname( __FILE__ ) . '/includes/activity-sharing.php' );
+}
+add_action( 'bp_include', 'bp_social_init' );
+
+    /**
+     * Enqueue buddy social styles
+     */
+    function buddy_social_icons_stylesheet() {
+      if (is_buddypress() ) {
+        // Respects SSL, Style.css is relative to the current file
+        wp_register_style( 'buddy-social', plugins_url('css/buddy-social.css', __FILE__) );
+        wp_enqueue_style( 'buddy-social' );
+      }
+    }
+
+    add_action( 'wp_enqueue_scripts', 'buddy_social_icons_stylesheet' );
+
+    // include the js for to toggle the social buttons
+    function buddy_social_scripts_method() {
+      if (is_buddypress() ) {
+        wp_enqueue_script(
+            'custom-script',
+            plugins_url('/js/buddy-social.js', __FILE__),
+            array( 'jquery' ),
+          	'1.0', 
+          	true
+        );
+    	}
+    }      
+    // enqueue the buddy social js
+    add_action( 'wp_enqueue_scripts', 'buddy_social_scripts_method' );
+
+?>
